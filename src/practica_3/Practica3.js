5e8f637a20eb528c0b2db4cd71f9d1d4a0bb20c5
Pablo Botta <pabloluisbotta@gmail.com>
2023-09-19 15:36:35 -0300

    modelo práctica 3

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -2,9 +2,20 @@ import { useState } from "react";
 
 
 export default function Practica3 ({ props }) { 
+    const [arr, setArr] = useState([]);
+    const onClickHandler = () => {
+        // let newArr = [...arr];
+        let nArr = [...arr, 'x'];
+        setArr(nArr);
+    }
     return (
         <div>
             <h3>Práctica 3</h3>
+            <input type="text" />
+            <button onClick={onClickHandler}>Agregar</button>
+            { arr.map((item) => (
+                <div>{item}</div>
+            ))}
         </div>
     );
 }
